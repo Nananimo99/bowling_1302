5a5b547b7beb1b4d26eb98bd2057de3649ceaa59
NOMOLOS15000 <Pruksanfp095@gmail.com>
2026-08-01 14:14:13 +0700

    Shoot commit

--- Assets/scripts/Bowling.cs
+++ Assets/scripts/Bowling.cs
@@ -34,7 +34,7 @@ public class Bowling : MonoBehaviour
         }
     }
 
-    private void ShootBall()
+    public void ShootBall()
     {
         rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);
     }
